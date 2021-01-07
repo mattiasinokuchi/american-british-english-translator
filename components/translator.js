50a9665166df857d4f4c885d549980be1e7ca9c9
Mattias Inokuchi <8-b@tuta.io>
2021-01-07 12:18:18 +0000

    completed test 'translate mangoes...'

--- components/translator.js
+++ components/translator.js
@@ -1,5 +1,5 @@
 const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
+const differences = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
@@ -7,11 +7,18 @@ class Translator {
 
   // Function for something exciting...
   toBritish (string) {
-    let translation = '';
+    let translation = [];
     let array = string.split(' ');
     console.log(array);
-
-    return translation;
+    array.forEach(function (element) {
+      if (differences.hasOwnProperty(element)) {
+        console.log(differences[element]);
+        translation.push(differences[element]);
+      } else {
+        translation.push(element);
+      }
+    });
+    return translation.join(' ');
   }
 
 }

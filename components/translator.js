4a3d44f5afbd6b44e36e91f469592778dab7a597
Mattias Inokuchi <8-b@tuta.io>
2021-01-08 13:29:32 +0000

    completed test 'translate like a...'

--- components/translator.js
+++ components/translator.js
@@ -6,67 +6,24 @@ const britishSlang = require('./british-only.js')
 class Translator {
 
   // Better luck this time
-  toBritishSlang(string) {
+  toBritish(string) {
     let translation = string;
     console.log(translation);
     Object.keys(americanSlang).forEach(function (element) {
-      let slang = new RegExp("\\b"+element+"\\b","g");
-      console.log(slang);
+      let slang = new RegExp("\\b"+element+"\\b","ig");
       let formal = americanSlang[element];
       translation = translation.replace(slang, formal);
     });
     console.log(translation);
     Object.keys(americanBritish).forEach(function (element) {
-      let american = new RegExp(element,"g");
+      let american = new RegExp("\\b"+element+"\\b","ig");
       let british = americanBritish[element];
       translation = translation.replace(american, british);
     });
-    console.log(translation);
-    /*Object.keys(britishSlang).forEach(function (element) {
-      let formal = new RegExp(element,"g");
-      let slang = britishSlang[element];
-      translation = translation.replace(formal, slang);
-    });*/
+    //console.log(translation);
     return translation;
   }
 
-  // This function turned out to be crap...
-/*  toBritish (string) {
-    // ...captures !?. ...
-    let endMark = string[string.length-1];
-    let translation = [];
-    let array = string
-      // ...removes end mark...
-      .slice(0, string.length-1)
-      // ...splits string to an array...
-      .split(' ');
-    console.log(string, array);
-    // ...each word is...
-    array.forEach(function (element) {
-      // ...checked for American slang...
-      if (americanSlang.hasOwnProperty(element)) {
-        console.log(americanSlang[element]);
-        // ...and changed to formal American english...
-        element = americanSlang[element];
-      }
-      // ...checked for American english...
-      if (english.hasOwnProperty(element)) {
-        console.log(english[element]);
-        // ...and translated to British english...
-        element = english[element];
-      }
-      // ...checked...
-      if (britishSlang.hasOwnProperty(element)) {
-        console.log(britishSlang[element]);
-        // ...and changed to British slang...
-        element = britishSlang[element];
-      }
-      // ...and added to the translation...
-      translation.push(element);
-    });
-    // ...returns translated sentence after adding the end mark
-    return translation.join(' ').concat(endMark);
-}*/
 
 }
 
